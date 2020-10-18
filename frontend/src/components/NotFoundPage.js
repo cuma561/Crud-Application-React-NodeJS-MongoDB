fd87d7d03a1c7112898eaf23d86c035df05dd43e
Bartek-Krawczak <cuma561@github.com>
2020-10-18 20:43:54 +0200

    Add Not found page files frontend application:

---
+++ frontend/src/components/NotFoundPage.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+class NotFoundPage extends Component{
+	render(){
+		return(
+			<div className="not-found">
+				<h1 className="display-1">Not Found Page</h1>
+			</div>
+		);
+	};
+}
+export default NotFoundPage
